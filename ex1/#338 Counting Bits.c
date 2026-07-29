fc27cdf8e00f4b3401148c25ac31cc791a0adead
Muthu Lekshmi .B <cbalacbala_bcs28@mepcoeng.ac.in>
2026-07-29 15:37:37 +0530

    Update #338 Counting Bits.c

--- ex1/#338 Counting Bits.c
+++ ex1/#338 Counting Bits.c
@@ -11,3 +11,21 @@ int* countBits(int n, int* returnSize) {
             ans[i]=ans[i/2]+1;
         }
     }
+
+
+Input: n = 2
+Output: [0,1,1]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+
+   Input: n = 5
+Output: [0,1,1,2,1,2]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+5 --> 101
