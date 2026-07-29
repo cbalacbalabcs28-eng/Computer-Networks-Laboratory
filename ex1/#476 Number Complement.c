958411962ef5c35a78f62f6037c837ab7f15085c
Muthu Lekshmi .B <cbalacbala_bcs28@mepcoeng.ac.in>
2026-07-29 15:38:57 +0530

    Update #476 Number Complement.c

--- ex1/#476 Number Complement.c
+++ ex1/#476 Number Complement.c
@@ -10,3 +10,11 @@ int findComplement(int num) {
     }
     return num ^ mask;
 }
+
+Input: num = 5
+Output: 2
+Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+
+Input: num = 1
+Output: 0
+Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
