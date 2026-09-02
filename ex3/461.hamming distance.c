609b1381c8637fbe5baa9f99ca622dcdb82c2137
Muthu Lekshmi .B <cbalacbala_bcs28@mepcoeng.ac.in>
2026-09-02 15:05:59 +0530

    Update 461.hamming distance.c

--- ex3/461.hamming distance.c
+++ ex3/461.hamming distance.c
@@ -10,3 +10,11 @@ int hammingDistance(int x, int y) {
     
     return distance;
 }
+
+Input: x = 1, y = 4
+Output: 2
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+The above arrows point to positions where the corresponding bits are different.
